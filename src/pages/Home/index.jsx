c3c6246ae779064b2aa5a6b008dc29c4fe8ab8c2
Eduardo Henrique <eduardo.hbds@gmail.com>
2024-06-01 19:31:27 -0300

    feat: added button in home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,16 @@
 import useAuth from "../../hooks/useAuth"
 import { useNavigate } from "react-router-dom";
 import * as C from "./style";
+import Button from "../../components/Button";
 const Home = () => {
   const {signout} = useAuth();
   const navigate = useNavigate();
   return (
     <C.Container>
       <C.Title>
-        
+        <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
+          Sair
+        </Button>
       </C.Title>
     </C.Container>
   )
